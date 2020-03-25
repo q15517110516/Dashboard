654f9130a5b683c178d4f8e9ec9e85457cfc40da
q15517110516 <lmrzds@gmail.com>
2020-03-25 18:46:52 -0400

    install react-icons

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import './SideBar.css';
+import 'antd/dist/antd.css';
+// import {
+//     DashboardOutlined
+// } from '@antd-design/icons';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import { PropTypes } from 'prop-types';
 // import PerfectScrollbar from 'perfect-scrollbar';
@@ -18,9 +22,7 @@ export class SideBar extends Component {
                 <nav className="navbar">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                        <svg class="bi bi-pie-chart-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M15.985 8.5H8.207l-5.5 5.5a8 8 0 0013.277-5.5zM2 13.292A8 8 0 017.5.015v7.778l-5.5 5.5zM8.5.015V7.5h7.485A8.001 8.001 0 008.5.015z"/>
-                        </svg>
+                            <icon type="dashboard"/>
                             <a className="nav-link" href="#">Home</a>
                         </li>
                         <li className="nav-item">
