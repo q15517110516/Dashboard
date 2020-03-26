d20dddf270dc71b9a7f89d90f83308c3432e85bd
q15517110516 <lmrzds@gmail.com>
2020-03-26 17:44:15 -0400

    update sidebar

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import './SideBar.css';
-import 'antd/dist/antd.css';
-// import {
-//     DashboardOutlined
-// } from '@antd-design/icons';
+import { AiOutlineDashboard, 
+    AiOutlineUser, 
+    AiOutlineHome, 
+    AiOutlineBarChart,
+    AiOutlineLineChart,
+    AiOutlinePieChart } from 'react-icons/ai';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import { PropTypes } from 'prop-types';
 // import PerfectScrollbar from 'perfect-scrollbar';
@@ -14,22 +16,51 @@ export class SideBar extends Component {
     constructor(props){
         super(props);
     }
-
+    
 
     render() {
         return (
             <div className="sidebar">
                 <nav className="navbar">
-                    <ul className="navbar-nav">
+                    <ul className="nav flex-column">
                         <li className="nav-item">
-                            <icon type="dashboard"/>
-                            <a className="nav-link" href="#">Home</a>
+                            
+                            <a className="nav-link" href="#">
+                                <AiOutlineHome className="icons home"/>
+                                <span>HOME</span>                         
+                            </a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Dashboard</a>
+                            <a className="nav-link" href="#">
+                                <AiOutlineUser className="icons user"/>
+                                <span>USERS</span>
+                            </a>
+                            
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Users</a>
+
+                        {/* Dropdown */}
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <AiOutlineDashboard className="icons dasnboard"/>
+                                <span>DASHBOARD</span>
+                            </a>
+                            
+                            {/* Dropdown Menu */}
+                            <div className="dropdown menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a className="nav-link dropdownitem" href="#">
+                                    <AiOutlineLineChart className="linechart-icon"/>
+                                    <span>LineChart</span>
+                                </a>
+                                <a className="nav-link dropdownitem" href="#">
+                                    <AiOutlineBarChart className="barchart-icon"/>
+                                    <span>BarChart</span>
+                                </a>
+                                <a className="nav-link dropdownitem" href="#">
+                                    <AiOutlinePieChart className="piechart-icon"/>
+                                    <span>PieChart</span>
+                                </a>
+                                
+                            </div>
                         </li>
                     </ul>
                 </nav>
