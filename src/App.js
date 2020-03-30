801a5e63828bfae9b282375ccb219ee6f6c506e1
q15517110516 <lmrzds@gmail.com>
2020-03-29 20:37:09 -0400

    complete router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-// import React, { Component } from 'react';
-// import Router from './Router';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Barchart from './components/DashBoard/BarChart/Barchart';
+import Linechart from './components/DashBoard/LineChart/LineChart';
+import Piechart from './components/DashBoard/PieChart/Piechart';
+import Home from './components/Home/Home';
+import Message from './components/Message/Message';
+import Notebook from './components/Notebook/Notebook';
+import Users from './components/Users/Users';
+import SideBar from './components/SideBar/SideBar';
+import './App.css';
 
 
-// class App extends Component {
-//     render() {
-//         return (
-//             <div>
-//                 <Router/>  
-//             </div>
-//         )
-//     }
-// }
+class App extends Component {
+    render() {
+        return (
+            <div className="flex-container">
+                <div className="sider">
+                    <SideBar/> 
+                </div>
+                <div className="content">
+                    <Switch>
+                        <Route exact path = "/" component={Home}/>
+                        <Route path = "/Users" component={Users}/>
+                        <Route path = "/Message" component={Message}/>
+                        <Route path = "/Notebook" component={Notebook}/>
+                        <Route path = "/Dashboard/Linechart" component={Linechart}/>
+                        <Route path = "/Dashboard/Barchart" component={Barchart}/>
+                        <Route path = "/Dashboard/Piechart" component={Piechart}/>
+                    </Switch>
+                </div>
+            </div>
+        )
+    }
+}
 
-// export default App;
+export default App;
