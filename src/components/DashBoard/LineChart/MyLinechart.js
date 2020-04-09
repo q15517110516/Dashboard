caafe39a15542fd6435071ebfda271eecced0f6d
q15517110516 <lmrzds@gmail.com>
2020-04-09 16:21:02 -0400

    2nd draft linechart

--- src/components/DashBoard/LineChart/MyLinechart.js
+++ src/components/DashBoard/LineChart/MyLinechart.js
@@ -9,6 +9,10 @@ export class MyLinechart extends Component {
         return (
             <div className="PerformanceSheet">
                 <div className="title">Performance Sheet</div>
+                <div className="currentuser">
+                    <img src={user.img} alt="user" style={{height: 72, width: 63, marginRight: 20}}/>
+                    <span>{user.name}</span>
+                </div>
                 <div className="mylinechart" style={{ overflowX: 'auto', overflowY: 'hidden' }}>
                     <LineChart data={user} width={1000} height={400}/>
                 </div>
--- src/components/DashBoard/LineChart/UserList.js
+++ src/components/DashBoard/LineChart/UserList.js
@@ -14,19 +14,20 @@ export class UserList extends Component {
             <div className="user-list">
                 <div className="title">User List</div>
                 <div className="list">
-                <List
-                size="large"
-                dataSource={data}
-                renderItem={user => 
-                    <List.Item className="user"  onClick={() => this.selectUser(user)}>
-                        <div>
-                        </div>
-                    
-                        <div>
-                            {user.name}
-                        </div>
-                    </List.Item>}
-                />  
+                    <List
+                    className="listofuser"
+                    size="large"
+                    dataSource={data}
+                    renderItem={user => 
+                        <List.Item className="user"  onClick={() => this.selectUser(user)}>
+                            <div style={{ width: "20%", height: 60, marginLeft: 20, marginTop: 10 }}>
+                                <img src={user.img} style={{width: 42, height: 48}} alt="user"/>
+                            </div>
+                            <div style={{ width: "80%", font: "20px sans-serif", marginLeft: 20, textAlign: "left"}}>
+                                {user.name}
+                            </div>
+                        </List.Item>}
+                    />  
                 </div>     
             </div>
         )
