54037834239ae334a5968d5703f0c34680f0a9de
q15517110516 <lmrzds@gmail.com>
2020-04-10 18:51:38 -0400

    complete modal

--- src/components/Users/MyUser.js
+++ src/components/Users/MyUser.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Modal, Button } from 'antd';
 import './MyUser.css';
 import data from '../../data';
+import "antd/dist/antd.css";
 
 export class MyUsers extends Component {
     
@@ -49,38 +50,35 @@ export class MyUsers extends Component {
                                     aria-hidden 
                                     alt="userphoto" 
                                     onClick={() => {this.changeSelectUser(user); this.showModal(true);}}/>
-                                
-                                {/* Click to display users info */}
-                                <Modal className="modal"
-                                    title="Personal Information"
-                                    centered
-                                    style={{ top: "50%", left: "50%"}}
-                                    visible={this.state.visible}
-                                    onCancel={() => this.showModal(false)}
-                                    footer={[
-                                        <Button key="cancel" onClick={() => this.showModal(false)}>Cancel</Button>
-                                    ]}
-                                    >
-                                    
-                                    <div className="personalInfo">
-                                        <p>Name: {selectUser.name}</p>
-                                        <p>Gender: {selectUser.gender}</p>
-                                        <p>Age: {selectUser.age}</p>
-                                        <p>ID: {selectUser.ID}</p>
-                                        <p>Contact Number: {selectUser.phone}</p>
-                                        <p>Email: {selectUser.email}</p>
-                                    </div>
-                                </Modal>
-                                
                             </div>
 
                             {/* User Name */}
                             <div className="username">
                                 {user.name}
                             </div>
-                            
                         </div>
                         ))}
+
+                        {/* Click to display users info */}
+                        <Modal
+                            title="Personal Information"
+                            centered
+                            visible={this.state.visible}
+                            onCancel={() => this.showModal(false)}
+                            footer={[
+                                <Button key="cancel" onClick={() => this.showModal(false)}>Cancel</Button>
+                            ]}
+                            >
+                            
+                            <div className="personalInfo">
+                                <p>Name: {selectUser.name}</p>
+                                <p>Gender: {selectUser.gender}</p>
+                                <p>Age: {selectUser.age}</p>
+                                <p>ID: {selectUser.ID}</p>
+                                <p>Contact Number: {selectUser.phone}</p>
+                                <p>Email: {selectUser.email}</p>
+                            </div>
+                        </Modal>
 
                     </div>
                 
