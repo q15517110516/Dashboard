b73f0e476bff4aa31f6e5403a4386e1ec413bc55
q15517110516 <lmrzds@gmail.com>
2020-04-10 20:13:44 -0400

    notebook updates

--- src/components/Notebook/Notebook.js
+++ src/components/Notebook/Notebook.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import './Notebook.css';
 import ToDoApp from './ToDoApp';
 
+
 export class Notebook extends Component {
     render() {
         return (
-            
-            <div className="notebook-todo">
-                <div className="title">ToDo List</div>
-                <div className="todoapp">
-                    <ToDoApp />
+            <div className="notebook">
+                <div className="notebook-todo">
+                    <div className="title">ToDo List</div>
+                    <div className="todoapp">
+                        <ToDoApp />
+                    </div>
                 </div>
+                
             </div>
             
         )
