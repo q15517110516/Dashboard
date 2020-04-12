4fc1d0bd9b1e0e10eb224930fce2e2bcb3b93e05
q15517110516 <lmrzds@gmail.com>
2020-04-12 18:52:38 -0400

    finished template of 4 boxes in home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,29 +3,40 @@ import Activities from './Activities';
 import './Home.css';
 import { AiOutlineCheckCircle, 
         AiOutlineDollar,
-        AiOutlineUserAdd, } from 'react-icons/ai';
+        AiOutlineUserAdd,
+        AiOutlineGold } from 'react-icons/ai';
 
 export class Home extends Component {
     render() {
         return (
             <div className="home">
                 <div className="home-boxes">
-                    <div className="totalIncome">
-                        <AiOutlineCheckCircle className="home-icons"/>
-                        <div>
-                        
+                    <div className="monthlyIncome">
+                        <AiOutlineDollar className="home-icons" style={{color: "#28A745"}}/>
+                        <div className="home-content">
+                            <span className="home-text">Monthly Income</span><br/>
+                            <span className="home-numbers">3.26M</span>
                         </div>
                     </div>
                     <div className="taskComplete">
-                        <AiOutlineDollar className="home-icons"/>
-                        <div>
-                        
+                        <AiOutlineCheckCircle className="home-icons" style={{color: "#E14ECA"}}/>
+                        <div className="home-content">
+                            <span className="home-text">Completed Tasks</span><br/>
+                            <span className="home-numbers">3,756</span>
                         </div>
                     </div>
                     <div className="newEmployees">
-                        <AiOutlineUserAdd className="home-icons"/>
-                        <div>
-                        
+                        <AiOutlineUserAdd className="home-icons" style={{color: "#1F8EF1"}}/>
+                        <div className="home-content">
+                            <span className="home-text">New Employees</span><br/>
+                            <span className="home-numbers">4,632</span>
+                        </div>
+                    </div>
+                    <div className="supplies">
+                        <AiOutlineGold className="home-icons" style={{color: "#DC3545"}}/>
+                        <div className="home-content">
+                            <span className="home-text">Supplies</span><br/>
+                            <span className="home-numbers">23,546</span>
                         </div>
                     </div>
 
