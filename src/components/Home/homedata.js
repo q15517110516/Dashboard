348410389b58ab66e79b98760ebe0a4d030c5f72
q15517110516 <lmrzds@gmail.com>
2020-04-13 16:10:57 -0400

    create data for linechart

---
+++ src/components/Home/homedata.js
@@ -0,0 +1,45 @@
+const taskCompleted = [
+    {"month": "Jan", "value": 316},
+    {"month": "Feb", "value": 275},
+    {"month": "Mar", "value": 284},
+    {"month": "Apr", "value": 283},
+    {"month": "May", "value": 372},
+    {"month": "Jun", "value": 218},
+    {"month": "Jul", "value": 328},
+    {"month": "Aug", "value": 372},
+    {"month": "Sept", "value": 295},
+    {"month": "Oct", "value": 175},
+    {"month": "Nov", "value": 365},
+    {"month": "Dec", "value": 265},
+
+]
+
+const newEmployees = [
+    {"month": "Jan", "value": 372},
+    {"month": "Feb", "value": 366},
+    {"month": "Mar", "value": 428},
+    {"month": "Apr", "value": 127},
+    {"month": "May", "value": 248},
+    {"month": "Jun", "value": 215},
+    {"month": "Jul", "value": 295},
+    {"month": "Aug", "value": 346},
+    {"month": "Sept", "value": 147},
+    {"month": "Oct", "value": 362},
+    {"month": "Nov", "value": 249},
+    {"month": "Dec", "value": 335},
+]
+
+const supplies = [
+    {"month": "Jan", "value": 249},
+    {"month": "Feb", "value": 435},
+    {"month": "Mar", "value": 255},
+    {"month": "Apr", "value": 254},
+    {"month": "May", "value": 374},
+    {"month": "Jun", "value": 246},
+    {"month": "Jul", "value": 346},
+    {"month": "Aug", "value": 386},
+    {"month": "Sept", "value": 326},
+    {"month": "Oct", "value": 146},
+    {"month": "Nov", "value": 253},
+    {"month": "Dec", "value": 271},
+]
