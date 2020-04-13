a72fe65abd2936fc7d92c2ebeb7bafd8f442d319
q15517110516 <lmrzds@gmail.com>
2020-04-12 20:47:29 -0400

    complete 4 boxes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,42 +5,49 @@ import { AiOutlineCheckCircle,
         AiOutlineDollar,
         AiOutlineUserAdd,
         AiOutlineGold } from 'react-icons/ai';
+import AnimatedNumber from 'react-animated-number';
 
 export class Home extends Component {
+
+    formatValue = value => `${parseInt(Number(value)).toLocaleString()}`;
+    
     render() {
         return (
             <div className="home">
+
+                {/* Four Boxes */}
                 <div className="home-boxes">
                     <div className="monthlyIncome">
                         <AiOutlineDollar className="home-icons" style={{color: "#28A745"}}/>
                         <div className="home-content">
                             <span className="home-text">Monthly Income</span><br/>
-                            <span className="home-numbers">3.26M</span>
+                            <AnimatedNumber className="home-numbers" value={13523400} duration={1500} formatValue={this.formatValue}/>
                         </div>
                     </div>
                     <div className="taskComplete">
                         <AiOutlineCheckCircle className="home-icons" style={{color: "#E14ECA"}}/>
                         <div className="home-content">
                             <span className="home-text">Completed Tasks</span><br/>
-                            <span className="home-numbers">3,756</span>
+                            <AnimatedNumber className="home-numbers" value={3756} duration={1500} formatValue={this.formatValue}/>
                         </div>
                     </div>
                     <div className="newEmployees">
                         <AiOutlineUserAdd className="home-icons" style={{color: "#1F8EF1"}}/>
                         <div className="home-content">
                             <span className="home-text">New Employees</span><br/>
-                            <span className="home-numbers">4,632</span>
+                            <AnimatedNumber className="home-numbers" value={4632} duration={1500} formatValue={this.formatValue}/>
                         </div>
                     </div>
                     <div className="supplies">
                         <AiOutlineGold className="home-icons" style={{color: "#DC3545"}}/>
                         <div className="home-content">
                             <span className="home-text">Supplies</span><br/>
-                            <span className="home-numbers">23,546</span>
+                            <AnimatedNumber className="home-numbers" value={23546} duration={1500} formatValue={this.formatValue}/>
                         </div>
                     </div>
-
                 </div>
+
+                {/* Activities */}
                 <div className="home-activities">
                     <div className="title">Activities</div>
                     <div className="activities">
