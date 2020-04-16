49b38a5b8d01795f6fdf135043cf10a73e30290c
q15517110516 <lmrzds@gmail.com>
2020-04-16 17:48:36 -0400

    modify format of month

--- src/components/Home/homedata.js
+++ src/components/Home/homedata.js
@@ -1,16 +1,16 @@
 const homedata = [
-    {"month": "Jan", "taskCompleted": 316, "newEmployees": 372, "supplies": 249, "monthlyIncome": 1738692,},
-    {"month": "Feb", "taskCompleted": 275, "newEmployees": 366, "supplies": 435, "monthlyIncome": 2716484,},
-    {"month": "Mar", "taskCompleted": 284, "newEmployees": 428, "supplies": 255, "monthlyIncome": 1836633,},
-    {"month": "Apr", "taskCompleted": 283, "newEmployees": 127, "supplies": 254, "monthlyIncome": 1827503,},
-    {"month": "May", "taskCompleted": 372, "newEmployees": 248, "supplies": 374, "monthlyIncome": 1275632,},
-    {"month": "Jun", "taskCompleted": 218, "newEmployees": 215, "supplies": 246, "monthlyIncome": 1827453,},
-    {"month": "Jul", "taskCompleted": 328, "newEmployees": 295, "supplies": 346, "monthlyIncome": 1725483,},
-    {"month": "Aug", "taskCompleted": 372, "newEmployees": 346, "supplies": 386, "monthlyIncome": 1237562,},
-    {"month": "Sept", "taskCompleted": 295, "newEmployees": 147, "supplies": 326, "monthlyIncome": 1243211, },
-    {"month": "Oct", "taskCompleted": 175, "newEmployees": 362, "supplies": 146, "monthlyIncome": 2243221,},
-    {"month": "Nov", "taskCompleted": 365, "newEmployees": 249, "supplies": 253, "monthlyIncome": 1927462,},
-    {"month": "Dec", "taskCompleted": 265, "newEmployees": 335, "supplies": 271, "monthlyIncome": 1123212,},
+    {"month": "2019-1-1", "taskCompleted": 316, "newEmployees": 372, "supplies": 249, "monthlyIncome": 1738692,},
+    {"month": "2019-2-1", "taskCompleted": 275, "newEmployees": 366, "supplies": 435, "monthlyIncome": 2716484,},
+    {"month": "2019-3-1", "taskCompleted": 284, "newEmployees": 428, "supplies": 255, "monthlyIncome": 1836633,},
+    {"month": "2019-4-1", "taskCompleted": 283, "newEmployees": 127, "supplies": 254, "monthlyIncome": 1827503,},
+    {"month": "2019-5-1", "taskCompleted": 372, "newEmployees": 248, "supplies": 374, "monthlyIncome": 1275632,},
+    {"month": "2019-6-1", "taskCompleted": 218, "newEmployees": 215, "supplies": 246, "monthlyIncome": 1827453,},
+    {"month": "2019-7-1", "taskCompleted": 328, "newEmployees": 295, "supplies": 346, "monthlyIncome": 1725483,},
+    {"month": "2019-8-1", "taskCompleted": 372, "newEmployees": 346, "supplies": 386, "monthlyIncome": 1237562,},
+    {"month": "2019-9-1", "taskCompleted": 295, "newEmployees": 147, "supplies": 326, "monthlyIncome": 1243211, },
+    {"month": "2019-10-1", "taskCompleted": 175, "newEmployees": 362, "supplies": 146, "monthlyIncome": 2243221,},
+    {"month": "2019-11-1", "taskCompleted": 365, "newEmployees": 249, "supplies": 253, "monthlyIncome": 1927462,},
+    {"month": "2019-12-1", "taskCompleted": 265, "newEmployees": 335, "supplies": 271, "monthlyIncome": 1123212,},
 
 ]
 
