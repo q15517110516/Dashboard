66940f1ba2adf4b416996a6dd3674c45be5be69f
q15517110516 <lmrzds@gmail.com>
2020-04-20 16:54:32 -0400

    finished adding circle & tooltip

--- src/charts/HomeChart/homechart.js
+++ src/charts/HomeChart/homechart.js
@@ -1,5 +1,6 @@
 import * as d3 from 'd3';
 import homedata from '../../components/Home/homedata';
+import './style.css';
 
 const draw = (props) => {
     d3.select(".vis-homechart > *").remove();
@@ -24,8 +25,19 @@ const draw = (props) => {
         d.supplies = +d.supplies;
 
     })
-    // let g = svg.append("g")
-    //     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // Add Tooltip
+    let tooltip = d3.select("body")
+                    .append("div")
+                    .attr("class", "tooltip-homechart");
+                    
+    // Append y grid lines
+    function y_gridline(){
+        return d3.axisLeft(yScale).ticks(10);
+    }
+
+
+    
 
     // Add Y axis
     let xScale = d3.scaleTime()
@@ -56,6 +68,18 @@ const draw = (props) => {
         .attr("stroke-width", 0)
         .style("font", "14px times");
 
+    // Add y grid lines
+    svg.append("g")
+        .attr("class", "grid")
+        .attr("transform", "translate(0," + width + ")")
+        .call(y_gridline()
+            .tickSize(-width)
+            .tickFormat("")
+        );
+
+    let g = svg.append("g")
+        // .attr("transform", "translate(" ")");
+
     // Append the line3
     let line1 = d3.line()
         .x(function(d){
@@ -84,61 +108,120 @@ const draw = (props) => {
 
     
     // Display the path
-    svg.append("path")
+    g.append("path")
         .datum(homedata)
-        .attr("class", "taskCompleted-line")
+        .attr("class", "homechart-line")
         .attr("d", line1)
         .attr("fill", "none")
         .attr("stroke", "#E14ECA")
         .attr("stroke-width", 4);
 
-    svg.append("path")
+    g.append("path")
         .datum(homedata)
-        .attr("class", "newEmployees-line")
+        .attr("class", "homechart-line")
         .attr("d", line2)
         .attr("fill", "none")
         .attr("stroke", "#1F8EF1")
         .attr("stroke-width", 4);
 
-    svg.append("path")
+    g.append("path")
         .datum(homedata)
-        .attr("class", "supplies-line")
+        .attr("class", "homechart-line")
         .attr("d", line3)
         .attr("fill", "none")
         .attr("stroke", "#DC3545")
         .attr("stroke-width", 4);
 
 
-    let focus = svg.append("g")
+    let focus = g.append("g")
         .attr("class", "focus")
         .style("display", "none");
-    
-    focus.append("line")
-        .attr("class", "x-hover-line hover-line")
-        .attr("y1", -height)
-        .attr("y2", (height - margin.top - margin.bottom))
-        .attr("stroke", "white")
-        .attr("stroke-width", 3);
 
-    // focus.append("line")
-    //     .attr("class", "y-hover-line hover-line")
-    //     .attr("x1", width)
-    //     .attr("x2", width)
-    //     .attr("stroke", "white")
-    //     .attr("stroke-width", 3);
-    
-    // focus.append("circle")
-    //     .attr("r", radius)
-    //     .style("color", "white");
 
-    svg.append("rect")
+    // Append circles to each line
+    g.selectAll(".dot-homechart")
+        .data(homedata)
+        .enter()
+        .append("circle")
+        .attr("class", ".dot-homechart")
+        .attr("cx", function(d){
+            return xScale(d.month);
+        })
+        .attr("cy", function(d){
+            return yScale(d.taskCompleted);
+        })
+        .attr("r", radius)
+        .attr("fill", "#E14ECA");
+
+
+    g.selectAll(".dot-homechart")
+        .data(homedata)
+        .enter()
+        .append("circle")
+        .attr("class", ".dot-homechart")
+        .attr("cx", function(d){
+            return xScale(d.month);
+        })
+        .attr("cy", function(d){
+            return yScale(d.newEmployees);
+        })
+        .attr("r", radius)
+        .attr("fill", "#1F8EF1");
+        
+
+    g.selectAll(".dot-homechart")
+        .data(homedata)
+        .enter()
+        .append("circle")
+        .attr("class", ".dot-homechart")
+        .attr("cx", function(d){
+            return xScale(d.month);
+        })
+        .attr("cy", function(d){
+            return yScale(d.supplies);
+        })
+        .attr("r", radius)
+        .attr("fill", "#DC3545");
+        
+
+    g.append("rect")
         .attr("class", "overlay")
         .attr("width", width)
         .attr("height", height)
         .attr("opacity", 0)
-        .on("mouseover", function() { focus.style("display", null); })
-        .on("mouseout", function() { focus.style("display", "none"); })
+        .on("mouseover", function() { 
+            focus.style("display", null);
+            
+            tooltip.transition()
+                .duration(200)
+                .style("opacity", 0.9); ; 
+        })
+        .on("mouseout", function() { 
+            focus.style("display", "none");
+            tooltip.transition()
+                .duration(200)
+                .style("opacity", 0); 
+        })
         .on("mousemove", mousemove);
+
+
+    // Vertical line
+    focus.append("line")
+        .attr("class", "x-hover-line hover-line")
+        .attr("y1", -height)
+        .attr("y2", height)
+        .attr("stroke", "white")
+        .attr("stroke-width", 3);
+
+    focus.append("rect")
+        .attr("class", "tooltip-homechart")
+        .attr("width", 100)
+            .attr("height", 50)
+            .attr("x", 10)
+            .attr("y", -22)
+            .attr("rx", 4)
+            .attr("ry", 4);
+    
 
     function mousemove() {
         let x0 = xScale.invert(d3.mouse(this)[0]),
@@ -146,14 +229,14 @@ const draw = (props) => {
             d0 = homedata[i - 1],
             d1 = homedata[i],
             d = x0 - d0.month > d1.month - x0 ? d1 : d0;
-        focus.attr("transform", "translate(" + xScale(d.month) + "," + yScale(d.taskCompleted) + "," + yScale(d.newEmployees) + ")");
-        // focus.attr("transform", "translate(" + xScale(d.month) + "," + yScale(d.newEmployees) + ")");
-        // focus.attr("transform", "translate(" + xScale(d.month) + "," + yScale(d.supplies) + ")");
-
-        // focus.select("text").text(function() { return d.value; });
-        // focus.select(".x-hover-line").attr("y2", height);
+        focus.attr("transform", "translate(" + xScale(d.month) + "," + yScale(d.taskCompleted) + ")");
+        focus.select(".x-hover-line").attr("y2", height);
         // focus.select(".y-hover-line").attr("x2", width + width);
+        tooltip.html(d3.timeFormat("%b")(d.month) + "<br />Task Completed: " + d.taskCompleted + "<br />New Employees: " + d.newEmployees + "<br />Supplies: " + d.supplies)
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY) + "px");
     }
+
 
 }
 
