c79ceac76fe3e3a91a7517e74acfceab751f7eb1
q15517110516 <lmrzds@gmail.com>
2020-05-14 15:11:08 -0400

    Updates

---
+++ precache-manifest.5875c45a7c98992eaa0d07aaaeed1192.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edd06386f6c07bc3fab7e15153548717",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc22673c1e08d02debad",
+    "url": "/static/css/2.b015a2ec.chunk.css"
+  },
+  {
+    "revision": "814ce824da58ed32d53d",
+    "url": "/static/css/main.358b3ea5.chunk.css"
+  },
+  {
+    "revision": "cc22673c1e08d02debad",
+    "url": "/static/js/2.361c19aa.chunk.js"
+  },
+  {
+    "revision": "07decc61612430e19097805bf1cf7a5b",
+    "url": "/static/js/2.361c19aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "814ce824da58ed32d53d",
+    "url": "/static/js/main.a0518f64.chunk.js"
+  },
+  {
+    "revision": "830e2d79836f9ebaf87a",
+    "url": "/static/js/runtime-main.5291cb3a.js"
+  }
+]);
